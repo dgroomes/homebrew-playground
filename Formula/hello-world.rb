a5d2311ac7b466fa06b3b9650a2b31591f05c147
David Groomes <dwgroomes@gmail.com>
2024-06-08 10:35:41 -0500

    Tip for running brew in the Ruby debugger

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -1,4 +1,23 @@
+require 'debug'
+
 # A simple Homebrew "formula" that installs a "hello world"-style shell script.
+#
+# Tip: debug a formula script by running Homebrew with the Ruby debugger: "rdbg". To accomplish this, I modified the
+# shell script that starts the "brew.rb" Ruby script. Specifically, I modified the "exec" command near the very bottom
+# of '/opt/homebrew/Library/Homebrew/brew.sh' to conditionally use "rdbg" instead of the normal Ruby interpreter.
+#
+#     if [[ "${HOMEBREW_RDBG}" == "true" ]]; then
+#       RUBYOPT="-W1 --disable=gems" exec "${HOMEBREW_LIBRARY}/Homebrew/vendor/portable-ruby/current/bin/rdbg" "${HOMEBREW_LIBRARY}/Homebrew/brew.rb" "$@"
+#     else
+#       exec "${HOMEBREW_RUBY_PATH}" "${HOMEBREW_RUBY_WARNINGS}" "${HOMEBREW_RUBY_DISABLE_OPTIONS}" "${HOMEBREW_LIBRARY}/Homebrew/brew.rb" "$@"
+#     fi
+#
+# Then, install this formula in a way that enables the Ruby debugger:
+#
+#     HOMEBREW_RDBG=true brew install dgroomes/homebrew-playground/hello-world
+#
+# Now you're in the Ruby debugger. Continue, step through, evaluate an expression, etc.
+#
 class HelloWorld < Formula
   desc "Say 'hello world'"
 
@@ -23,6 +42,7 @@ class HelloWorld < Formula
   # The `install` method defines the remaining logic to get the software fully installed on the computer. In this case,
   # we're installing a single script called `hello-world.zsh` and renaming it to `hello` in the `bin` directory.
   def install
+    binding.break
     bin.install "hello-world.zsh" => "hello-world"
   end
 
